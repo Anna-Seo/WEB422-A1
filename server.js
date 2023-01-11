6f6628239d2f51c120ac56206114f3f85c6fd907
Anna Seo <aseo5@myseneca.ca>
2023-01-11 15:20:48 -0500

    commit2

--- server.js
+++ server.js
@@ -9,17 +9,13 @@
 *
 ********************************************************************************/
 const express = require("express");
+const bodyparser = require("body-parser");
 const app = express();
 var cors = require('cors');
 const MoviesDB = require("./modules/moviesDB.js");
-const { DBRef } = require("bson");
 const db = new MoviesDB();
 
 const HTTP_PORT = process.env.PORT || 8080;
-
-function onHttpStart() {
-    console.log("Express http server listening on: " + HTTP_PORT);
-}
 
 app.use(cors());
 app.use(express.json());
@@ -31,7 +27,6 @@ app.get("/", (req, res) => {
 
 app.post("/api/movies", (req, res) => {
     db.addNewMovie(req.body).then((data) => {
-        console.log(data);
         res.json(data);
     }).catch((err) => {
         console.log(err);
@@ -51,7 +46,6 @@ app.get("/api/movies", (req, res) => {
 
 app.get("/api/movies/:_id", (req, res) => {
     db.getMovieById(req.params._id).then((data) => {
-        console.log("get");
         res.json(data);
     }).catch((err) => {
         console.log(err);
@@ -60,7 +54,6 @@ app.get("/api/movies/:_id", (req, res) => {
 
 app.put("/api/movie/:_id", (req, res) => {
     db.updateMovieById(req.body, req.params._id).then((data) => {
-        console.log("update");
         res.json(data);
     }).catch((err) => {
         console.log(err);
@@ -69,7 +62,6 @@ app.put("/api/movie/:_id", (req, res) => {
 
 app.delete("/api/movies/:_id", (req, res) => {
     db.deleteMovieById(req.params._id).then((data) => {
-        console.log("delete");
         res.json(data);
     }).catch((err) => {
         console.log(err);
@@ -78,7 +70,7 @@ app.delete("/api/movies/:_id", (req, res) => {
 
 db.initialize(process.env.MONGODB_CONN_STRING)
     .then(() => {
-        app.listen(HTTP_PORT, onHttpStart);
+        app.listen(HTTP_PORT);
     }).catch((err) => {
         console.log(err);
     });
